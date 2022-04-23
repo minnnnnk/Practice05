5c6f31e817ea9321a398a1e97f4187c0bdcbfcbc
김민규 <@DESKTOP-AUV4F3L>
2022-04-23 22:58:29 +0900

    Ex01

--- src/com/javaex/ex01/MemberApp.java
+++ src/com/javaex/ex01/MemberApp.java
@@ -4,7 +4,26 @@ public class MemberApp {
 
 	public static void main(String[] args) {
 
+		Member jung = new Member();
+		jung.setId("jws");
+		jung.setName("정우성");
+		jung.setPoint(50000);
 		
+		Member yoo = new Member();
+		
+		yoo.setId("yjs");
+		yoo.setName("유재석");
+		yoo.setPoint(30000);
+		
+		Member lee = new Member();
+		
+		lee.setId("lhr");
+		lee.setName("이효리");
+		lee.setPoint(40000);
+		
+		jung.showInfo();
+		yoo.showInfo();
+		lee.showInfo();
 	}
 
 }
