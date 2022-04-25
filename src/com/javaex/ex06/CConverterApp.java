2ceeb75344a09f12dc8cd472f40a9d7141632356
mingyu <jubal9153@gmail.com>
2022-04-25 14:57:10 +0900

    Ex06

--- src/com/javaex/ex06/CConverterApp.java
+++ src/com/javaex/ex06/CConverterApp.java
@@ -5,16 +5,14 @@ public class CConverterApp {
 
     public static void main(String[] args) {
 
-        double dollar;
-        double won;
         
         CConverter.setRate(1118.70);
         
         //100만원을 달러로 출력하기
-
+        System.out.println(CConverter.toDoller(1000000)+"달러");
         
         //100달려를 원으로 출력하기
-        
+        System.out.println(CConverter.toKWR(100)+"원");
         
     }
 
